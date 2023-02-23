98b06f33ee4f0ef63c12da6f17c28ad82ec366f0
pantuoferm <pantuofermo@gmail.com>
2023-02-23 12:03:02 -0500

    - small fix and changes to env variables/files

--- env.d.ts
+++ env.d.ts
@@ -4,16 +4,15 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_NJ_SITE_URL: string;
-  readonly VITE_TX_SITE_URL: string;
-  // more env variables...
-
+  readonly VITE_TX_SITE_URL: string;  
   readonly VITE_INVOICEGENERATOR_URL: string;
+  readonly VITE_IMAGE_REPOSITORY_URL: string;
+  readonly VITE_REPAIRFORM_URL: string;
+
   readonly VITE_INVOICE_TRACKING_URL_FORMAT_AIRBORN: string;
   readonly VITE_INVOICE_TRACKING_URL_FORMAT_UPS: string;  
   readonly VITE_INVOICE_TRACKING_URL_FORMAT_FEDEX: string;
-  readonly VITE_IMAGE_REPOSITORY_URL: string;
-
-  readonly VITE_REPAIRFORM_URL: string;
+  
 }
 
 interface ImportMeta {
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,13 @@ import { useAuthStore } from '@/stores/auth-store';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
   routes: [
     // {
     //   path: '/',
